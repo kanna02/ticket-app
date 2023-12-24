a7276bc079c6aaf24caee42a4133a7cb6d87cafa
kanna02 <Anna.Kantorczyk@city.ac.uk>
2023-12-24 11:48:28 +0100

    react-router-dom-added, app.js removed, pages setup

---
+++ src/pages/my-account/MyAccountPage.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+function MyAccountPage(props) {
+
+    return (
+        <div>
+            <h1>My Account Page</h1>
+        </div>
+    );
+}
+
+export default MyAccountPage;
---
+++ src/pages/project-board/ProjectBoardPage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function ProjectBoardPage(props) {
+
+    return (
+        <div>
+            <h1>Project Board</h1>
+        </div>
+    );
+}
+
+export default ProjectBoardPage;
