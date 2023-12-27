f441259a33085fe2001f7f9cf6e3b5e077fd39b3
Levi <levi.benoliel-elliott@city.ac.uk>
2023-12-27 11:25:15 +0100

    dropdown

--- src/App.js
+++ src/App.js
@@ -4,17 +4,37 @@ import { Button } from './components/button/Button';
 import { NavBar } from './components/headerbar/HeaderBar';
 import { ProjectCard } from './components/project-card/ProjectCard';
 import { TextInput } from './components/text-input/TextInput';
+import { Dropdown } from './components/dropdown/Dropdown';
 
 function App() {
   // button js interaction
   const handleButtonClick = () => {
     alert('Button clicked!');
   };
+
+  const [selectedValue, setSelectedValue] = useState('');
+
+  const handleDropdownChange = (event) => {
+    setSelectedValue(event.target.value);
+  };
+
+  const dropdownOptions = [
+    { label: 'Option 1', value: '1' },
+    { label: 'Option 2', value: '2' },
+    { label: 'Option 3', value: '3' },
+  ];
+  
   return (
     <div className="App">
 
+    {/* <Dropdown
+    label="Select an Option:"
+        options={dropdownOptions}
+        onChange={handleDropdownChange}
+      /> */}
+
 {/* Interaction Button */}
-    {/* <Button/> */}
+    <Button/>
 
 {/* HeaderBar */}
     {/* <NavBar/> */}
---
+++ src/components/dropdown/Dropdown.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import "../../App.css"
+
+export const Dropdown = () => {
+  return (
+    <div className="dropdown-container">
+      <label>{label}</label>
+      <select onChange={onChange} className="dropdown">
+        {options.map((option, index) => (
+          <option key={index} value={option.value}>{option.label}</option>
+        ))}
+      </select>
+    </div>
+  )
+}
