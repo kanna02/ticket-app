c25a479f27653449f128b894a1c15c9b13d62116
kanna02 <Anna.Kantorczyk@city.ac.uk>
2023-12-29 13:57:39 +0100

    create popup with button + input components, moved button + input css to own css files

---
+++ src/components/text-input/TextInput.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import "./TextInput.css"
+
+
+export const TextInput = () => {
+    
+  return (
+    <div>
+      <label >
+        <input className='input-text' name="input"/>
+      </label>
+    </div>
+  )
+}
