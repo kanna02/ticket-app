f0d315243e9d84e78ab338f167c8ac3ec903a821
kanna02 <Anna.Kantorczyk@city.ac.uk>
2024-01-05 01:07:59 +0100

    added api post call to create new ticket

--- client/src/components/project-popup/ProjectPopup.jsx
+++ client/src/components/project-popup/ProjectPopup.jsx
@@ -14,29 +14,29 @@ export function ProjectPopup(props) {
             <div className="row">
                 <div className="label">Title</div>
                 <div className="input">
-                    <TextInput/>
+                    <TextInput value={props.title} setValue={props.setTitle}/>
                 </div>
             </div>
             <div className="row">
                 <div className="label">Description</div>
                 <div className="input">
-                    <TextInput description/>
+                    <TextInput description value={props.description} setValue={props.setDescription}/>
                 </div>
             </div>
             <div className="row">
                 <div className="label">Deadline</div>
                 <div className="input">
-                    <Date/>
+                    <Date value={props.completionDate} setValue={props.setCompletionDate}/>
                 </div>
             </div>
             <div className="row">
                 <div className="label">Members</div>
                 <div className="input">
-                    <TextInput/>
+                    <TextInput value={props.members} setValue={props.setMembers}/>
                 </div>
             </div> 
             <div className="buttons">
-                <Button save text="Save"/>
+                <Button save text="Save" onClick={props.saveOnClick}/>
                 <Button cancel text="Cancel" onClick={props.cancelOnClick}/>
             </div>   
         </div>
--- client/src/pages/my-projects/MyProjectsPage.jsx
+++ client/src/pages/my-projects/MyProjectsPage.jsx
@@ -5,7 +5,18 @@ import { useState } from "react";
 import './MyProjectsPage.css';
 import { ProjectPopup } from "../../components/project-popup/ProjectPopup";
 import { Button } from "../../components/button/Button";
+import axios from "axios";
+
+
 function MyProjectsPage(props) {
+
+    const [showProjectPopup, setShowProjectPopup] = useState(false);
+    const [projectTitle, setProjectTitle] = useState("");
+    const [projectDescription, setProjectDescription] = useState("");
+    const [projectCompletionDate, setProjectCompletionDate] = useState(null);
+    const [projectMembers, setProjectMembers] = useState("");
+
+    //TODO: get this from api/get/projects
     const projects = [
         { name: "Project Name 1", description: "This form allows you to generate random text strings", participants: 4, completionDate: "23.01.2024" },
         { name: "Project Name 1", description: "This form allows you to generate random text strings", participants: 4, completionDate: "23.01.2024" },
@@ -13,46 +24,76 @@ function MyProjectsPage(props) {
         { name: "Project Name 1", description: "This form allows you to generate random text strings", participants: 4, completionDate: "23.01.2024" },
     ];
 
-    const handleAddProjectClick = () => {
-        openPopup();
-    };
-
-    const [showProjectPopup, setShowProjectPopup] = useState(false);
-
     const closePopup = () => {
         setShowProjectPopup(false);
     }
 
-    // use this for + button onclick
     const openPopup = () => {
         setShowProjectPopup(true);
     }
+
+    const saveProject = () => {
+        // throw error if fields empty
+        if (projectTitle === "" || projectDescription === "" || projectMembers === "" || projectCompletionDate == null) {
+            alert("error, not all inputs have values");
+        }
+        else {
+            // format date
+            const date = new Date(projectCompletionDate);
+            const completionDateFormatted = `${date.getFullYear()}-${date.getMonth()+1}-${date.getDate()}`
+            
+            const baseURL = 'https://db-api-dot-task-master-409210.nw.r.appspot.com/api/post/project';
+            const body = {
+                title: projectTitle,
+                description: projectDescription,
+                completion_date: completionDateFormatted
+            }
+            axios.post(baseURL, body
+                ).then((response) => {
+                    console.log(response);
+                }).catch(function (error) {
+                console.log(error);
+                });
+
+            closePopup();
+        }
+    }
+
+
 
     return (
         <div className="my-projects-container">
             <NavBar/>
             <div className="top-container">
-            <div className="projects-headers">
-                <h1>My Projects</h1>
-                <p>Welcome back, select your project.</p>
-            </div>
-            <div className="header-actions">
-                    <Button round onClick={handleAddProjectClick} />             
+                <div className="projects-headers">
+                    <h1>My Projects</h1>
+                    <p>Welcome back, select your project.</p>
+                </div>
+                <div className="header-actions">
+                    <Button round onClick={openPopup}/>             
                 </div>
             </div>
-                <div className="projects-container">
-            {projects.map((project, index) => (
-                <ProjectCard key={index} name={project.name}
-                        projectDescription={project.description}
+            <div className="projects-container">
+                {projects.map((project, index) => (
+                    <ProjectCard 
+                        key={index} 
+                        name={project.name}                          
+                        projectDescription={project.description}                   
                         participants={project.participants}
                         completionDate={project.completionDate}
                     />
-            ))}
-        </div>
-        {showProjectPopup && <ProjectPopup cancelOnClick={closePopup}/>}
-        </div>
-
-        
+                ))}
+            </div>
+        {showProjectPopup && 
+            <ProjectPopup 
+                title={projectTitle} setTitle={e => setProjectTitle(e.target.value)} 
+                description={projectDescription} setDescription={e => setProjectDescription(e.target.value)} 
+                completionDate={projectCompletionDate} setCompletionDate={setProjectCompletionDate} 
+                members={projectMembers} setMembers={e => setProjectMembers(e.target.value)} 
+                cancelOnClick={closePopup} saveOnClick={saveProject}
+            />
+        }
+        </div>       
     );
 }
 export default MyProjectsPage;
