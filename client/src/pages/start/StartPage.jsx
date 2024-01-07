2848a9a86b04252b909fcc29eb4bb3846517e88f
kanna02 <Anna.Kantorczyk@city.ac.uk>
2024-01-07 01:39:29 +0100

    merge conflicts fixed

--- client/src/pages/start/StartPage.jsx
+++ client/src/pages/start/StartPage.jsx
@@ -9,23 +9,13 @@ import { useNavigate } from 'react-router-dom';
 function StartPage() {
   const navigate = useNavigate();
 
-  const [loginResponse, setLoginResponse ] = useState([]);
-  const [loginProfile, setLoginProfile ] = useState([]);
+  const [loginResponse, setLoginResponse ] = useState(null);
+  const [loginProfile, setLoginProfile ] = useState(null);
 
-  // google login
-  const login = useGoogleLogin({
-    onSuccess: (codeResponse) => setLoginResponse(codeResponse),
-    onError: (error) => console.log('Login Failed:', error)
-  });
-
-  // console.log(loginResponse.length);
-  console.log(loginResponse);
-
-  console.log(loginResponse.access_token ? "true" : "false");
+  const clientId = "975454066980-4g7e706rtl1ukvctquj9g5ubg3cbvgrq.apps.googleusercontent.com"; //TODO: save to environment file
 
   useEffect(() => {
     if (loginResponse?.access_token) {
-        console.log("Access Token:", loginResponse.access_token); // Debug log
         axios.get(`https://www.googleapis.com/oauth2/v1/userinfo?access_token=${loginResponse.access_token}`, {
             headers: {
                 Authorization: `Bearer ${loginResponse.access_token}`,
@@ -33,7 +23,6 @@ function StartPage() {
             },
         })
         .then((res) => {
-            console.log("User Data:", res.data); // Debug log
             setLoginProfile(res.data);
         })
         .catch((err) => {
@@ -41,21 +30,26 @@ function StartPage() {
         });
     }
   }, [loginResponse]);
- 
-
-  const handleLogin = () => {
-    console.log(loginProfile)
-      login();
-  };
 
   useEffect(() => {
     if (loginProfile) {
         const data = { email: loginProfile.email}
         localStorage.setItem('myData', JSON.stringify(data)); // Save data to localStorage
-        navigate("/my-projects", { state: { email: loginProfile.email } });
+        navigate("/my-projects");
     }
   }, [loginProfile, navigate]);
- 
+
+  // google login
+  const login = useGoogleLogin({
+    onSuccess: (codeResponse) => setLoginResponse(codeResponse),
+    onError: (error) => console.log('Login Failed:', error),
+    clientId: clientId,
+  });
+
+  const handleLogin = () => {
+    console.log(loginProfile) 
+    login();
+  };
 
   return (
     <div className="App">
