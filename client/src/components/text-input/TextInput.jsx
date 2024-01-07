1ce6aca330f3b1157e04a99e38e006b2884f2790
kanna02 <Anna.Kantorczyk@city.ac.uk>
2024-01-07 01:46:41 +0100

    deleted login component(unused), added text-input disabled support

--- client/src/components/text-input/TextInput.jsx
+++ client/src/components/text-input/TextInput.jsx
@@ -5,7 +5,7 @@ export const TextInput = (props) => {
   return (
     <div className='input-containers'>
       {props.label && <label className='input-label'>{props.label}</label>}
-      <input className={props.description ? 'input-description' : 'input-text'} value={props.value} onChange={props.setValue} name={props.name} placeholder={props.placeholder} />
+      <input disabled={props.disabled} className={props.description ? 'input-description' : 'input-text'} value={props.value} onChange={props.setValue} name={props.name} placeholder={props.placeholder} />
     </div>
   );
 };
